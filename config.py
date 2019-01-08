5459031fc6d9860d49cc999f46b45311602dea8a
SergeiRing <sereshka0967@gmail.com>
2019-01-08 14:26:58 +0300

    small changes in config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Configuration():
 	DEBUG = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://mysql:mysql@127.0.0.1/DocsHub'
+	SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://username:password@host/DocsHub'
 	SECRET_KEY = 'very very secret'
 	SECURITY_PASSWORD_SALT = 'salt'
 	SECURITY_PASSWORD_HASH = 'des_crypt'
